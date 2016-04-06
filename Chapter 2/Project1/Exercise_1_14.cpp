6ee7fa6e7cba6b57ffbec808ffe876ddcf5c163e
Ramadanov <sh.ramadanov@gmail.com>
2016-04-06 19:11:28 +0300

    file update

---
+++ Chapter 2/Project1/Exercise_1_14.cpp
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+ int zad14()
+{
+
+
+	char words[100];
+	int  ascii_symbol[127] = { 0 };
+
+
+	printf("Please write something: \n");
+	fgets(words, sizeof words, stdin);
+
+	for (int i = 0; i<100; i++)
+	{
+		if ((words[i] != NULL) && ((int)words[i] != 10) && ((int)words[i] != 0) && ((int)words[i] != -52))
+		{
+			ascii_symbol[(int)words[i]]++;
+			//printf("%d\t", (int)words[i]);
+			//printf("%d\t", ascii_symbol[(int)words[i]]);
+			//printf("\n");
+
+		}
+	}
+
+	for (int i = 0; i < 128; i++)
+	{
+		if (ascii_symbol[i]>0)
+		{
+			//printf("%d\n", i);
+			char temp;
+			temp = (char)i;
+			printf("%c", temp);
+			for (int j = 0; j < ascii_symbol[i]; j++)
+			{
+				printf("*");
+			}
+			printf("\n");
+		}
+	}
+	getchar();
+	return 0;
+}
