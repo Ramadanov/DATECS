854e75c2110cc462167e6ec4d9328f349b8cbead
Ramadanov <sh.ramadanov@gmail.com>
2016-04-08 11:53:29 +0300

    2_3
    still some issues with random input symbols
    for example 0xAsDgK
    but calculation works unless its go above memory reserve
    6xf(fffff) returns '-1'

--- Testing/Project_Ex_2_3/Ex_2_3_test/Ex_2_3_test.cpp
+++ Testing/Project_Ex_2_3/Ex_2_3_test/Ex_2_3_test.cpp
@@ -1,64 +1,36 @@
 #include <stdio.h>
 
 int power(int, int);
+int hotoi(char[], int);
 
 int main()
 {
 
 	char hexadec[100];
-	char hexadecreverse[100];
+//	char hexadecreverse[100];
 	int fin_dec = 0, count=0;
+//	int dec_converted;
 	//int i = 2;
 	printf("Enter Your Hexadecimal:\t");
 	fgets(hexadec, sizeof hexadec, stdin);
 	
 	//printf("%c\n", hexadec[i]);
 
-	int intconverter;
+	//int intconverter;
 	//temp = hexadec[i] - '0'; //(int)hexadec[1];
 	//printf("%d\n", temp);
-
-	for (int i = 0; (i < (sizeof hexadec) && hexadec[i] != '\n'); i++)
-	{
-		if ((hexadec[i] == 'a') || (hexadec[i] == 'A'))
-		{
-			fin_dec = (fin_dec + 10) * 16;
-		}
-		else if ((hexadec[i] == 'b') || (hexadec[i] == 'B'))
-		{
-			fin_dec = (fin_dec + 11) * 16;
-		}
-		else if ((hexadec[i] == 'c') || (hexadec[i] == 'C'))
-		{
-			fin_dec = (fin_dec + 12) * 16;
-		}
-		else if ((hexadec[i] == 'd') || (hexadec[i] == 'D'))
-		{
-			fin_dec = (fin_dec + 13) * 16;
-		}
-		else if ((hexadec[i] == 'e') || (hexadec[i] == 'E'))
-		{
-			fin_dec = (fin_dec + 14) * 16;
-		}
-		else if ((hexadec[i] == 'f') || (hexadec[i] == 'F'))
-		{
-			fin_dec = (fin_dec + 15) * 16;
-		}
-		else
-		{
-			fin_dec = (fin_dec + (hexadec[i] - '0')) * 16;
-
-		}
-	}
-	fin_dec = fin_dec / 16;
+	fin_dec = hotoi(hexadec, 100);
+	
 	printf("\nConverted to decimal:\t%d\n", fin_dec);
-	/*for (int i = count-1; i >0; i--)
+
+	/*
+	for (int i = count-1; i >0; i--)
 	{
 		intconverter = hexadec[i] - '0';
 		fin_dec = fin_dec + (intconverter)* power(16, i);
 	}
 
-	
+
 	/*
 	printf("[0] - > %c\n", c[0]);
 	printf("[1] - > %c\n", c[1]);
@@ -71,6 +43,7 @@ int main()
 	printf("temp - > %d\n", temp);
 
 	/*
+
 	char hexadec[2];
 	//unsigned char to_int;
 	int i=0;
@@ -80,10 +53,77 @@ int main()
 		printf("[0] -> %c\n", hexadec[0]);
 		printf("[1] ->%c\n", hexadec[1]);
 		
-		*/
-		getchar();
+	*/
+
+	getchar();
 	
 	return 0;
+}
+
+int hotoi(char hexadec[], int size)
+{
+	int dec_converted=0;
+	
+	//printf("\size:\t%d\n", size);
+	for (int i = 0; (i < (size) && hexadec[i] != '\n' && hexadec[i] != '\0'); i++)
+	{
+		if (hexadec[0] == '0')
+		{
+			i++;
+			if (hexadec[1] == 'x')
+			{
+				i++;
+			}
+		}
+		//printf("\i:\t%d\n", i);
+		//printf("\hexadec[i]:\t%d\n", hexadec[i]);
+		if ((hexadec[i] == 'a') || (hexadec[i] == 'A'))
+		{
+			//printf("\A\t\n");
+			dec_converted = (dec_converted + 10) * 16;
+			//printf("\dec_converted   :\t%d\n", dec_converted);
+		}
+		else if ((hexadec[i] == 'b') || (hexadec[i] == 'B'))
+		{
+			dec_converted = (dec_converted + 11) * 16;
+			//printf("\dec_converted   :\t%d\n", dec_converted);
+		}
+		else if ((hexadec[i] == 'c') || (hexadec[i] == 'C'))
+		{
+			dec_converted = (dec_converted + 12) * 16;
+			//printf("\dec_converted   :\t%d\n", dec_converted);
+		}
+		else if ((hexadec[i] == 'd') || (hexadec[i] == 'D'))
+		{
+			dec_converted = (dec_converted + 13) * 16;
+			//printf("\dec_converted   :\t%d\n", dec_converted);
+		}
+		else if ((hexadec[i] == 'e') || (hexadec[i] == 'E'))
+		{
+			dec_converted = (dec_converted + 14) * 16;
+			//printf("\dec_converted   :\t%d\n", dec_converted);
+		}
+		else if ((hexadec[i] == 'f') || (hexadec[i] == 'F'))
+		{
+			dec_converted = (dec_converted + 15) * 16;
+			//printf("\dec_converted   :\t%d\n", dec_converted);
+		}
+		else if (hexadec[i] == '0' || hexadec[i] == '1' || hexadec[i] == '2' || hexadec[i] == '3' || hexadec[i] == '4' || hexadec[i] == '5' || hexadec[i] == '6' || hexadec[i] == '7' || hexadec[i] == '8' || hexadec[i] == '9')
+		{
+			dec_converted = (dec_converted + (hexadec[i] - '0')) * 16;
+			//printf("\dec_converted   :\t%d\n", dec_converted);
+		}
+		else
+		{
+			printf("\*%c: -> Is not a valid HEX so it won't be counted!!!!!!!!!!!!!!!!!\n", hexadec[i]);
+		}
+		printf("\hexadec[%d]:\t%c\n", i, hexadec[i]);
+	}
+	dec_converted = dec_converted / 16;
+
+	//printf("\nConverted to decimal:\t%d\n", dec_converted);
+
+	return dec_converted;
 }
 
 int power(int X, int P)
