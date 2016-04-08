76f65c67333843e3d81ff02d074fa6f115aaa77d
Ramadanov <sh.ramadanov@gmail.com>
2016-04-08 10:09:41 +0300

    fixing issues with 2_2test

--- Testing/Project_Ex_2_2/Test_2_2/Ex_2_2_test.cpp
+++ Testing/Project_Ex_2_2/Test_2_2/Ex_2_2_test.cpp
@@ -3,18 +3,31 @@
 
 int main()
 {	
-	int lim = 5;
-	int c;
+	int lim = 100;	//limit for input symbols
+	int c;	//for old logic
+	int c1;		//for new logic
 
-	printf("Please inpute some line:\n");
-	c = getchar();
+	/*
+	printf("Inpute limit:\n");		//logig for input from keyboard;
+	lim = getchar();
+	lim = (lim - '0');
+	lim++;
+	*/
+
+
+
+	printf("\nPrint first %d symbols with * between them (from input):\n\n", lim  - 1);
 
 	
-//	printf("Print first %d symbols with * between them (from input):\n", lim-1);
-
-	//printf("usein: 'for (int i = 0;(i < lim - 1) && (c = getchar()) != EOF; ++i)' :\n");
-	/*
-	printf("%c*", (char)c);
+/*
+	//***********************old logic***************************
+	printf("usein: 'for (int i = 0;(i < lim - 1) && (c = getchar()) != EOF; ++i)' :\n");
+	
+	printf("Please inpute some line:\n");
+	
+	c = getchar();
+	
+	//printf("%c*", (char)c);
 	for (int i = 0;(i < lim - 1) && (c = getchar()) != EOF; ++i)
 	{
 		printf("%c", (char)c);
@@ -24,32 +37,49 @@ int main()
 
 		}
 	}
+	//***********************************************************
 */
+
+
+
+	//***************8*******new logic***************************
 
 	printf(" usein: for (int i = 0;(i < lim - 1); ++i)\n");
 	printf("{\n");
-	printf("\tif (c = getchar() != (EOF)) :\n");
-	printf("%c*", (char)c);
-	for (int i = 1;(i < lim - 1); ++i)
+	printf("\tif (c = getchar() != (EOF)) :\n\n\t\t");
+
+	printf("Please inpute some line:\n");
+	c1 = getchar();
+	//printf("%c*", (char)c);
+	for (int i = 0;(i < lim - 1); i++)
 	{
-		c = getchar();
-		if (c != (EOF))
+		if (c1 != (EOF))
 		{
-			printf("%c", (char)c);
-			if (c != '\n')
+			printf("%c", (char)c1);
+			c1 = getchar();
+			if (c1 != '\n')
 			{
-				if(i < lim - 1)
+				if (i < lim - 1)
 				{
 					printf("*");
 				}
 			}
-			
+			else
+			{
+				printf("\n");
+				break;
+			}
+
 		}
 		else
 		{
-			//break;
+			printf("\n");
+			break;
 		}
+
 	}
+	//***********************************************************
+
 	getchar();
 	return 0;
 }
